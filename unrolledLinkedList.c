0513fb29a3ea9e50e22b3094458b2fc7fa4afbd2
Nitros <estebanchecot@gmail.com>
2026-03-11 16:17:49 +0100

    Compact function can free pages with bitmap equal zero (no data)

--- unrolledLinkedList.c
+++ unrolledLinkedList.c
@@ -3,76 +3,101 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NB_ELEM 8
+#define PAGE_CAPACITY 8
+#define INIT_VALUE -1
 
 typedef struct Page Page;
 
 struct Page {
-  int element[NB_ELEM];
+  int value[PAGE_CAPACITY];
   uint8_t bitmap;
   Page *next;
 };
 
-typedef struct Position {
-  int element;
+typedef struct UnrolledList {
+  int pageCapacity;
+  int itemCount;
+  int pageCount;
+  Page *head;
+} UnrolledList;
+
+typedef struct ElementLocation {
+  int value;
   uint8_t index;
   Page *pageRef;
-} Position;
+} ElementLocation;
 
-typedef struct Occurences {
-  Position *founds;
+typedef struct SearchResult {
+  ElementLocation *founds;
   int number;
-} Occurences;
+} SearchResult;
 
-Page *constructorInit();
-void insert(Page **firstPage, int element);
-void removePosition(Page **firstPage, Position position);
-Occurences research(Page **firstPage, int element);
-void compact(Page **firstPage);
-void printPositions(Occurences occurences);
-void printAllPage(Page *first);
-void freeAllPages(Page *first);
+//////////////////////////////////////////////////////////////////////
+
+UnrolledList constructorListInit();
+Page *constructorPageInit();
+
+void insert(UnrolledList *list, int value);
+void removePosition(UnrolledList *list, ElementLocation position);
+void compact(UnrolledList *list);
+
+SearchResult research(UnrolledList *list, int value);
+
+void printLocations(SearchResult occurrences);
+void printUnrolledList(UnrolledList *list);
+
+void freeAllPages(Page *head);
 
 //////////////////////////////////////////////////////////////////////
 
 int main(int argc, char const *argv[]) {
-  printf("\n\n\n");
-
   srand(time(NULL));
-  Page *first = NULL;
+
+  UnrolledList list = constructorListInit();
 
   for (int i = 0; i < 7; i++) {
-    insert(&first, rand() % 15);
+    insert(&list, rand() % 15);
   }
 
-  Occurences occ = research(&first, 5);
+  SearchResult occ = research(&list, 5);
 
-  printAllPage(first);
+  printUnrolledList(&list);
 
   for (int i = 0; i < occ.number; i++) {
-    removePosition(&first, occ.founds[i]);
+    removePosition(&list, occ.founds[i]);
   }
+
   free(occ.founds);
 
-  printAllPage(first);
+  printUnrolledList(&list);
 
-  compact(&first);
-  printAllPage(first);
+  compact(&list);
+  printUnrolledList(&list);
 
-  freeAllPages(first);
-  printf("\n\n\n");
+  freeAllPages(list.head);
 
+  printf("\n\n");
   return 0;
 }
 
 //////////////////////////////////////////////////////////////////////
 
-Page *constructorInit() {
+UnrolledList constructorListInit() {
+  UnrolledList tmpList;
+  tmpList.head = constructorPageInit();
+  tmpList.itemCount = 0;
+  tmpList.pageCount = 1;
+  tmpList.pageCapacity = PAGE_CAPACITY;
+
+  return tmpList;
+}
+
+Page *constructorPageInit() {
   Page *tmpPage = malloc(sizeof(Page));
 
   tmpPage->bitmap = 0;
-  for (int i = 0; i < NB_ELEM; i++) {
-    tmpPage->element[i] = -1;
+  for (int i = 0; i < PAGE_CAPACITY; i++) {
+    tmpPage->value[i] = INIT_VALUE;
   }
   tmpPage->next = NULL;
 
@@ -81,64 +106,68 @@ Page *constructorInit() {
 
 //////////////////////////////////////////////////////////////////////
 
-void insert(Page **firstPage, int element) {
-  if (*firstPage == NULL) {
-    *firstPage = constructorInit();
+void insert(UnrolledList *list, int value) {
+  if (list->head == NULL) {
+    list->head = constructorPageInit();
+    list->pageCount++;
   }
 
-  Page *current = *firstPage;
+  Page *current = list->head;
 
   while (current != NULL) {
-    for (int i = 0; i < NB_ELEM; i++) {
+    for (int i = 0; i < PAGE_CAPACITY; i++) {
       if (!(current->bitmap & (1 << i))) {
-        current->element[i] = element;
+        current->value[i] = value;
         current->bitmap |= (1 << i);
+        list->itemCount++;
         return;
       }
     }
     if (current->next == NULL) {
-      current->next = constructorInit();
+      current->next = constructorPageInit();
+      list->pageCount++;
     }
     current = current->next;
   }
 }
 
-void removePosition(Page **firstPage, Position position) {
-  if (*firstPage == NULL) {
-    printf("Error : NULL firstPage");
+void removePosition(UnrolledList *list, ElementLocation position) {
+  if (list->head == NULL) {
+    printf("Error : \"head\" pointer is NULL");
     return;
   }
 
-  position.pageRef->element[position.index] = 0;
+  position.pageRef->value[position.index] = 0;
   position.pageRef->bitmap &= ~(1 << position.index);
+  list->itemCount--;
 }
 
-Occurences research(Page **firstPage, int element) {
-  Occurences occurences;
-  occurences.founds = NULL;
-  occurences.number = 0;
+SearchResult research(UnrolledList *list, int value) {
+  SearchResult occurrences;
+  occurrences.founds = NULL;
+  occurrences.number = 0;
 
-  if (*firstPage == NULL) {
-    printf("Error : NULL firstPage");
-    return occurences;
+  if (list->head == NULL) {
+    printf("Error : \"head\" pointer is NULL");
+    return occurrences;
   }
 
-  Position *founds = NULL;
-  Page *current = *firstPage;
+  ElementLocation *founds = NULL;
+  Page *current = list->head;
   int count = 0;
 
   while (current != NULL) {
-    for (int i = 0; i < NB_ELEM; i++) {
+    for (int i = 0; i < PAGE_CAPACITY; i++) {
       if (current->bitmap & (1 << i)) {
-        if (current->element[i] == element) {
+        if (current->value[i] == value) {
           count++;
           if (founds == NULL) {
-            founds = malloc(sizeof(Position));
+            founds = malloc(sizeof(ElementLocation));
           } else {
-            founds = realloc(founds, count * sizeof(Position));
+            founds = realloc(founds, count * sizeof(ElementLocation));
           }
 
-          Position tmp = {tmp.element = element, tmp.index = i, tmp.pageRef = current};
+          ElementLocation tmp = {.value = value, .index = i, .pageRef = current};
           founds[count - 1] = tmp;
         }
       }
@@ -147,42 +176,44 @@ Occurences research(Page **firstPage, int element) {
   }
 
   if (count == 0) {
-    printf("Alert : Element (%d) not found.\n\n", element);
-    return occurences;
+    printf("Alert : Value (%d) not found\n\n", value);
+    return occurrences;
   }
 
-  occurences.founds = founds;
-  occurences.number = count;
+  occurrences.founds = founds;
+  occurrences.number = count;
 
-  return occurences;
+  return occurrences;
 }
 
-void compact(Page **firstPage) {
-  if (*firstPage == NULL) {
+void compact(UnrolledList *list) {
+  if (list->head == NULL) {
+    printf("Error : \"head\" pointer is NULL");
+
     return;
   }
 
+  Page *current = list->head;
   int moved;
   do {
     moved = 0;
-
-    Page *current = *firstPage;
-    Position refHoleFound;
     int isRefFound = 0;
+    current = list->head;
+    ElementLocation refHoleFound;
 
     while (current != NULL) {
-      for (int i = 0; i < NB_ELEM; i++) {
-        if ((current->bitmap & (1 << i)) == 0) {
+      for (int i = 0; i < PAGE_CAPACITY; i++) {
+        if (!(current->bitmap & (1 << i))) {
           if (!isRefFound) {
-            refHoleFound.index = i;
             refHoleFound.pageRef = current;
+            refHoleFound.index = i;
             isRefFound = 1;
           }
         } else if (isRefFound) {
-          refHoleFound.pageRef->element[refHoleFound.index] = current->element[i];
+          refHoleFound.pageRef->value[refHoleFound.index] = current->value[i];
           refHoleFound.pageRef->bitmap |= (1 << refHoleFound.index);
 
-          current->element[i] = 0;
+          current->value[i] = 0;
           current->bitmap &= ~(1 << i);
 
           refHoleFound.index = i;
@@ -193,31 +224,53 @@ void compact(Page **firstPage) {
       current = current->next;
     }
   } while (moved);
+
+  Page *previous = NULL;
+  current = list->head;
+
+  while (current != NULL) {
+    if (current->bitmap == 0) {
+      Page *next = current->next;
+
+      if (previous == NULL) {
+        list->head = next;
+      } else {
+        previous->next = next;
+      }
+
+      free(current);
+      list->pageCount--;
+      current = next;
+    } else {
+      previous = current;
+      current = current->next;
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////////////////
 
-void printPositions(Occurences occurences) {
+void printLocations(SearchResult occurrences) {
   int count = 0;
-  for (int i = 0; i < occurences.number; i++) {
-    printf("Position found :\n\n");
-    printf("Element : %d\n", occurences.founds[count].element);
-    printf("Bitmap Index : %d\n", occurences.founds[count].index);
-    printf("Adress : %p\n\n", occurences.founds[count].pageRef);
+  printf("Position found :\n\n");
+  for (int i = 0; i < occurrences.number; i++) {
+    printf("Value : %d\n", occurrences.founds[count].value);
+    printf("Bitmap Index : %d\n", occurrences.founds[count].index);
+    printf("Adress : %p\n\n", occurrences.founds[count].pageRef);
     count++;
   }
 }
 
-void printAllPage(Page *first) {
-  Page *current = first;
+void printUnrolledList(UnrolledList *list) {
+  Page *current = list->head;
   int currentPageIndex = 0;
   while (current != NULL) {
     printf("Page %d\n\n", currentPageIndex);
-    for (int i = 0; i < NB_ELEM; i++) {
+    for (int i = 0; i < PAGE_CAPACITY; i++) {
       if (current->bitmap & (1 << i)) {
-        printf("Elem %d : %d\n", i, current->element[i]);
+        printf("Value %d : %d\n", i, current->value[i]);
       } else {
-        printf("Elem %d : (vide)\n", i);
+        printf("Value %d : (empty)\n", i);
       }
     }
     printf("\n");
@@ -228,8 +281,8 @@ void printAllPage(Page *first) {
 
 //////////////////////////////////////////////////////////////////////
 
-void freeAllPages(Page *first) {
-  Page *current = first;
+void freeAllPages(Page *head) {
+  Page *current = head;
   while (current != NULL) {
     Page *next = current->next;
     free(current);
